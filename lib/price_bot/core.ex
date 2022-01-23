40560a0f15f281a3d5636126eabc939e08eefd68
diegofigs <diego@dfigueroa.me>
2022-01-23 01:06:35 -0400

    :recycle: (lib) tweak core to take tickers instead of ids as input

--- lib/price_bot/core.ex
+++ lib/price_bot/core.ex
@@ -3,13 +3,33 @@ require Logger
 defmodule PriceBot.Core do
   @coingecko_api "https://api.coingecko.com/api/v3"
   @usd "usd"
+
+  @tickers %{
+    "btc" => "bitcoin",
+    "eth" => "ethereum",
+    "bnb" => "binancecoin",
+    ada: "cardano",
+    sol: "solana",
+    luna: "terra-luna",
+    dot: "polkadot",
+    avax: "avalanche-2",
+    matic: "matic-network",
+    atom: "cosmos",
+    near: "near",
+    ftm: "fantom",
+    one: "harmony",
+    osmo: "osmosis",
+    celo: "celo"
+  }
+
   def fetch(ticker, opts \\ %{currency: @usd}) do
+    requiredParams = constructParams(ticker, opts)
     optionalParams = %{
       include_market_cap: Map.get(opts, :market_cap, false),
       include_24hr_vol: Map.get(opts, :volume, false),
       include_24hr_change: Map.get(opts, :change, false)
     }
-    params = constructParams(ticker, opts) |> Map.merge(optionalParams)
+    params = requiredParams |> Map.merge(optionalParams)
 
     query_params = URI.encode_query(params)
     price_url = "#{@coingecko_api}/simple/price?#{query_params}"
@@ -19,7 +39,7 @@ defmodule PriceBot.Core do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         body
         |> Poison.decode!()
-        |> Map.get(ticker)
+        |> Map.get(Map.get(params, :ids))
         |> transform(opts)
       {:error, _} -> %{}
     end
@@ -51,7 +71,8 @@ defmodule PriceBot.Core do
   end
 
   def market_cap(ticker) do
-    fetch(ticker, %{market_cap: true})
+    opts = %{market_cap: true}
+    fetch(ticker, opts)
     |> Map.get(optionToField(:market_cap))
   end
 
@@ -67,7 +88,7 @@ defmodule PriceBot.Core do
 
   defp constructParams(ticker, opts) do
     %{
-      ids: ticker,
+      ids: Map.get(@tickers, ticker, ticker),
       vs_currencies: Map.get(opts, :currency, @usd)
     }
   end
