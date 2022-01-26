eb5a780a427de65ba9557e8dccdbf2940eeb90e2
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-01-26 00:48:22 -0400

    :recycle: (test) refactor tests for matching return types

--- test/price_bot/core_test.exs
+++ test/price_bot/core_test.exs
@@ -3,48 +3,52 @@ defmodule PriceBotCoreTest do
   use ExUnit.Case
   @btc "bitcoin"
 
-  test "fetch/1 requests bitcoin price and returns a number" do
-    assert PriceBot.Core.fetch(@btc) > 0
+  describe "fetch/1" do
+    test "returns a map from a coingecko-id" do
+      assert PriceBot.Core.fetch("bitcoin") |> is_map()
+    end
+
+    test "returns a map from a whitelisted ticker" do
+      assert PriceBot.Core.fetch("btc") |> is_map()
+    end
   end
 
-  test "fetch/1 requests bitcoin price with ticker as input" do
-    assert PriceBot.Core.fetch("btc") > 0
-  end
+  describe "fetch/2" do
+    test "returns a map with price, market_cap" do
+      assert PriceBot.Core.fetch(@btc, %{market_cap: true}) |> is_map()
+    end
 
-  test "fetch/2 requests price with market_cap" do
-    assert PriceBot.Core.fetch(@btc, %{market_cap: true}) > 0
-  end
+    test "returns a map with price, 24h_volume" do
+      assert PriceBot.Core.fetch(@btc, %{volume: true}) |> is_map()
+    end
 
-  test "fetch/2 requests price with 24h_volume" do
-    assert PriceBot.Core.fetch(@btc, %{volume: true}) > 0
-  end
+    test "returns a map with price, 24h_change" do
+      assert PriceBot.Core.fetch(@btc, %{change: true}) |> is_map()
+    end
 
-  test "fetch/2 requests price with 24h_change" do
-    assert PriceBot.Core.fetch(@btc, %{change: true}) > 0
-  end
-
-  test "fetch/2 requests price, mc, 24h volume and change" do
-    result = PriceBot.Core.fetch(@btc, %{market_cap: true, volume: true, change: true})
-    assert result > 0
+    test "returns a map with price, mc, 24h volume and change" do
+      result = PriceBot.Core.fetch(@btc, %{market_cap: true, volume: true, change: true})
+      assert result |> is_map()
+    end
   end
 
   test "price/1 returns a number" do
     result = PriceBot.Core.price(@btc)
-    assert result > 0
+    assert result |> is_number()
   end
 
   test "market_cap/1 returns a number" do
     result = PriceBot.Core.market_cap(@btc)
-    assert result > 0
+    assert result |> is_number()
   end
 
   test "volume/1 returns a number" do
     result = PriceBot.Core.volume(@btc)
-    assert result > 0
+    assert result |> is_number()
   end
 
   test "change/1 returns a number" do
     result = PriceBot.Core.change(@btc)
-    assert result
+    assert result |> is_number()
   end
 end
